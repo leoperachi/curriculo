abfb3f1a0e66ff71531283518de74badc66562e2
Leonardo <leoperachi@gmail.com>
2024-03-26 20:00:23 -0300

    vai

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 
 import './App.css';
+import { FaUserPen } from "react-icons/fa6";
+import { IoLocationSharp } from "react-icons/io5";
+import { MdWorkHistory } from "react-icons/md";
+import { PiStudentFill } from "react-icons/pi";
 
 function App() {
+  function clickBtnSideBar(event: any): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-     </h1>
+    <div className="flex h-screen justify-center items-center" style={{ backgroundSize: 'cover', backgroundPosition: 'center'  }}>
+      <div className="home-tab-nav" style={{height: '86vh', width: '53px', backgroundColor: 'rgb(31 31 31)' }}>
+        <button id='btnPersonal' className='btnIcon' 
+          onClick={clickBtnSideBar}><FaUserPen className='iconButton' />
+        </button>
+        <button id='btnEducation' className='btnIcon' 
+          onClick={clickBtnSideBar}><PiStudentFill className='iconButton'/>
+        </button>
+        <button id='btnLocation' className='btnIcon' 
+          onClick={clickBtnSideBar}><IoLocationSharp className='iconButton'/>
+        </button>
+        <button id='btnProfXp' className='btnIcon'
+          onClick={clickBtnSideBar}><MdWorkHistory className='iconButton'/>
+        </button>
+      </div>
     </div>
   );
 }
